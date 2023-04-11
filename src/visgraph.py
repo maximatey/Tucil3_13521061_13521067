fed9ac8ab9b75b3dd2f44eebf58a0eb5fc77f821
maximatey <maximun154@gmail.com>
2023-04-11 22:40:39 +0700

    final non bonus

--- src/visgraph.py
+++ src/visgraph.py
@@ -12,13 +12,13 @@ def drawGraph(graph, path, adj):
     for i in range(len(graph)):
         for j in range(i+1, len(graph)):
             if adj[i][j] != 0:
-                G.add_edge(i, j)
+                G.add_edge(i, j, weight = graph[i][j])
 
     # Gambar graf dengan jalur terpendek
     pos = nx.spring_layout(G)
     nx.draw(G, pos, with_labels=True, node_size=250)
     labels = nx.get_edge_attributes(G, 'weight')
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels, font_size=10)
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=labels, font_size=5)
     path_edges = list(zip(path, path[1:]))
-    nx.draw_networkx_edges(G, pos, edgelist=path_edges, edge_color='r', width=2)
+    nx.draw_networkx_edges(G, pos, edgelist=path_edges, edge_color='r', width=4)
     plt.show()
